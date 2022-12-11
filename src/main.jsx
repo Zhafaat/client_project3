6e8862b327183995b06dc40c651629dcd2f52975
Muh Hafis Tafsani <53614945+hafis915@users.noreply.github.com>
2022-12-11 22:08:58 +0700

    left side done

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom"
 import router from './router'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
